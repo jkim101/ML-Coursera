456cb235fbb554e4b06106711727d85268d21aea
jkim101 <jhkim4hg@gmail.com>
2017-01-10 20:40:05 -0500

    FINAL_W02

--- W02/EX1/computeCost.m
+++ W02/EX1/computeCost.m
@@ -6,16 +6,16 @@ function J = computeCost(X, y, theta)
 % Initialize some useful values
 m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 J = 0;
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
-
-
-
+  predictions = X*theta;  % predictions of hypothesis on all m examples
+  sqrErrors = (predictions - y).^2; % squared errors
+  J = (1/(2*m) * sum(sqrErrors));
 
 % =========================================================================
 
